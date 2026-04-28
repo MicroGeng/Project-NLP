34c7d005c270c14cadcf0427c82410039f42035d
David Pratama Chandra <david.gendutkeren@outlook.co.id>
2026-04-28 16:03:44 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Home from './mainComponent/home'
+
+function App() {
+  return (
+    <div className="App">
+      {/* Cukup panggil komponennya saja */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
